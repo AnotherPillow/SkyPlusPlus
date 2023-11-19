b7da407cd1eb4819d4fb19201fa215a7cbed091c
AnotherPillow <Thepilous_@outlook.com>
2023-11-19 14:34:45 +0000

    Update server IP lists with Noobcrew's NameMC likes

--- src/main/java/com/anotherpillow/skyplusplus/util/Server.java
+++ src/main/java/com/anotherpillow/skyplusplus/util/Server.java
@@ -5,7 +5,40 @@ import java.util.List;
 
 public class Server {
     public static boolean isSkyblock(String domain) {
-        List<String> skyblockDomains = Arrays.asList("skyblock.net", "skyblock.org", "skyblock.com", "mc.skyblock.net", "skyblock.in", "skyblock.ninja", "skyblock.info", "c.skyblock.net", "skyblock.rocks", "skyblock.one", "skyblock.biz");
+        List<String> skyblockDomains = Arrays.asList(
+                "skyblock.net",
+                "skyblock.org",
+                "skyblock.com",
+                "mc.skyblock.net",
+                "skyblock.in",
+                "skyblock.ninja",
+                "skyblock.info",
+                "c.skyblock.net",
+                "skyblock.rocks",
+                "skyblock.one",
+                "skyblock.biz",
+                "server.skyblock.net",
+                "playskyblocknetwork.com",
+                "skyblock-hub.com",
+                "skyblock.city",
+                "skyblock.click",
+                "skyblock.cloud",
+                "skyblock.cool",
+                "skyblock.date",
+                "skyblock.fun",
+                "skyblock.info",
+                "skyblock.men",
+                "skyblock.sbs",
+                "skyblock.today",
+                "skyblock.trade",
+                "skyblock.work",
+                "skyblock.zone",
+                "skyblocke.com",
+                "skyblockgames.com",
+                "skyblockia.com",
+                "skyblockia.org",
+                "skyblockia.net"
+        );
 
         return skyblockDomains.contains(domain);
 
