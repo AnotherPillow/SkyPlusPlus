bdcfe0e5b156238000fdbfb0e0917bdb7b4846cb
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2024-08-15 16:46:31 +0100

    Actually check for lowercommands

--- src/main/java/com/anotherpillow/skyplusplus/mixin/ChatScreenMixin.java
+++ src/main/java/com/anotherpillow/skyplusplus/mixin/ChatScreenMixin.java
@@ -1,5 +1,6 @@
 package com.anotherpillow.skyplusplus.mixin;
 
+import com.anotherpillow.skyplusplus.config.SkyPlusPlusConfig;
 import net.minecraft.client.gui.screen.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +11,8 @@ public class ChatScreenMixin {
     @ModifyArg(method={"keyPressed"},
             at=@At(value="INVOKE", target="Lnet/minecraft/client/gui/screen/ChatScreen;sendMessage(Ljava/lang/String;Z)Z"), index=0)
     private String result(String text) {
-        if (text.startsWith("/")) {
+        SkyPlusPlusConfig config = SkyPlusPlusConfig.configInstance.getConfig();
+        if (text.startsWith("/") && config.lowerCommandsEnabled) {
             String command = text.split(" ")[0];
             text = text.replace(command, command.toLowerCase());
         }
