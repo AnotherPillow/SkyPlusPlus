2a265890be4629b0e5cb9b4cddb32305ceb3b9f1
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2025-01-26 19:57:42 +0000

    Update HandledScreenMixin.java

--- src/main/java/com/anotherpillow/skyplusplus/mixin/HandledScreenMixin.java
+++ src/main/java/com/anotherpillow/skyplusplus/mixin/HandledScreenMixin.java
@@ -1,12 +1,14 @@
 package com.anotherpillow.skyplusplus.mixin;
 
 import com.anotherpillow.skyplusplus.keybinds.HoverNBTCopy;
+import com.anotherpillow.skyplusplus.util.Chat;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -15,13 +17,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HandledScreen.class)
-public abstract class HandledScreenMixin<T extends ScreenHandler> {
+public abstract class HandledScreenMixin<T extends ScreenHandler> extends Screen implements ScreenHandlerProvider<T> {
     @Shadow
     protected T handler;
 
+    // WILL BE IGNORED. DO NOT USE
+    protected HandledScreenMixin(Text title) {
+        super(title);
+    }
+
     @Invoker("isPointOverSlot")
     protected abstract boolean invokeIsPointOverSlot(Slot slot, double pointX, double pointY);
-
 
     @Inject(method = "Lnet/minecraft/client/gui/screen/ingame/HandledScreen;render(Lnet/minecraft/client/util/math/MatrixStack;IIF)V", at = @At("HEAD"))
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
@@ -30,6 +36,12 @@ public abstract class HandledScreenMixin<T extends ScreenHandler> {
             var slot = this.handler.slots.get(k);
             if (this.invokeIsPointOverSlot(slot, (double) mouseX, (double) mouseY)) {
                 HoverNBTCopy.hoveredItem = slot.getStack();
+
+                /* for future reference: get the title.
+                if (this.title != null) {
+
+                    Chat.send(this.title);
+                }*/
             }
         }
     }
